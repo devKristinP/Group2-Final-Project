fd728a9691aab52daa794226c565082ff655da3c
KristinP <7knicole@gmail.com>
2022-09-06 13:30:02 -0600

    Main

--- js/index.js
+++ js/index.js
@@ -1,37 +1,40 @@
 //console.log('Hello World');
-
+const taskManager = new TaskManager(1);
+taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
+taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
+console.log(taskManager.tasks);
 function formValidation(e) {
-    const name = document.getElementById('name')
-    const description = document.getElementById('description')
-    const assignedTo = document.getElementById('assignedTo')
-    const dueDate = document.getElementById('dueDate')
-    // const form = document.getElementById('form')
-    const errorElement = document.getElementById('error')
+  const name = document.getElementById("name");
+  const description = document.getElementById("description");
+  const assignedTo = document.getElementById("assignedTo");
+  const dueDate = document.getElementById("dueDate");
+  // const form = document.getElementById('form')
+  const errorElement = document.getElementById("error");
 
-    let messages = [ ]
-    if (name.value === '' || name.value == null){
-        messages.push('Name is required')
-    }
+  let messages = [];
+  if (name.value === "" || name.value == null) {
+    messages.push("Name is required");
+  }
 
-    if(description.value === '' || description.value == null){
-        messages.push('Enter Description for task')
-    }
+  if (description.value === "" || description.value == null) {
+    messages.push("Enter Description for task");
+  }
 
-    if(assignedTo.value === '' || assignedTo.value == null){
-        messages.push('Task must be assigned')
-    }
+  if (assignedTo.value === "" || assignedTo.value == null) {
+    messages.push("Task must be assigned");
+  }
 
-    if(dueDate.value === '' || dueDate.value == null){
-        messages.push('Enter a due date')
-    }
+  if (dueDate.value === "" || dueDate.value == null) {
+    messages.push("Enter a due date");
+  }
 
-    if(messages.length > 0){
-        e.preventDefault()
-        errorElement.innerHTML = messages.join(', ')
-    }
-};
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerHTML = messages.join(", ");
+  }
+}
 
-form.addEventListener('submit', formValidation);
+form.addEventListener("submit", formValidation);
 
 const newTask = new TaskManager();
 console.log(newTask);
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,10 +1,25 @@
 class TaskManager {
 
     constructor(currentId = 0){
-        this.tasks = [];
+        this.tasks = []; //this is the task list
         this.currentId = currentId;
     }
 
-    
+     // Method-- creating a task object and the key value pairs
+    addTask(name, description, assignedTo, dueDate) {
+        const task = {
+            id: this.currentId++, 
+            name: name, 
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: 'Todo',
+        }   
+        this.tasks.push(task);
+        /*currently returning first task with ID of zero, will eventually show number increasing*/
+    }
 
 };
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi"); */
