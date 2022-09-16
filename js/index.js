2750876406fad84457180ae815a7f44b1cf098af
KristinP <devKristinP@gmail.com>
2022-09-16 16:35:46 -0600

    fix tasks show and change button

--- js/index.js
+++ js/index.js
@@ -9,93 +9,86 @@
 */
 const taskManager = new TaskManager(0);
 
-
-taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
-taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
-console.log(taskManager.tasks);
-
-  
+//taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
+//taskManager.addTask("Laundry", "fold", "Bob", "12/31/2022");
+//console.log(taskManager.tasks);
 
 //const tasksList = document.querySelector('#tasksList');
 
 // Select the New Task Form
 // Select the New Task Form
-const newTaskForm = document.querySelector('#newTaskForm');
+const newTaskForm = document.querySelector("#newTaskForm");
 
 /// Add an 'onsubmit' event listener
-newTaskForm.addEventListener('submit', (event) => {
+newTaskForm.addEventListener("submit", (event) => {
   // Prevent default action
   event.preventDefault();
+  const formData = new FormData(event.currentTarget);
 
-    
+  for (const p of formData) {
+    console.log(p);
+  }
 
-    // Select the inputs
-    // Select the inputs
-    const newTaskName = document.querySelector('#newTaskNameInput');
-    const newTaskDescription = document.querySelector('#newTaskDescription');
-    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-    const newTaskDueDate = document.querySelector('#newTaskdueDate');
+  // Select the inputs
+  // Select the inputs
+  const newTaskName = document.querySelector("#newTaskNameInput");
+  const newTaskDescription = document.querySelector("#newTaskDescription");
+  const newTaskAssignedTo = document.querySelector("#newTaskAssignedTo");
+  const newTaskDueDate = document.querySelector("#newTaskDueDate");
 
-    
-    
-       // Validation code here - 
-    
-        let messages = [];
-        if (newTaskName.value == ""/* || newTaskName.value == null*/) {
-          messages.push("Name is required");
-        }
-      
-        if (newTaskDescription.value == "" /*|| newTaskDescription.value == null*/) {
-          messages.push("Enter Description for task");
-        }
-      
-        if (newTaskAssignedTo.value == "" /*|| newTaskAssignedTo.value == null*/) {
-          messages.push("Task must be assigned");
-        }
-      
-       if (newTaskDueDate.value == "" /*|| newTaskDueDate.value == null*/) {
-          messages.push("Enter a due date");
-        }
-      
-        if (messages.length > 0) {
-          e.preventDefault();
-          const errorElement = document.querySelector('#errorElement');
-          errorElement.innerHTML = messages.join(", ");
-        }
-        
-    // Get the values of the inputs
-    const name = newTaskNameInput.value;
-    const description = newTaskDescription.value;
-    const assignedTo = newTaskAssignedTo.value;
-    const dueDate = newTaskDueDate.value;
-    /*if(!validFormFieldInput(name)){
+  // Validation code here -
+
+  let messages = [];
+  if (newTaskName.value == "" /* || newTaskName.value == null*/) {
+    messages.push("Name is required");
+  }
+
+  if (newTaskDescription.value == "" /*|| newTaskDescription.value == null*/) {
+    messages.push("Enter Description for task");
+  }
+
+  if (newTaskAssignedTo.value == "" /*|| newTaskAssignedTo.value == null*/) {
+    messages.push("Task must be assigned");
+  }
+
+  if (newTaskDueDate.value == "" /*|| newTaskDueDate.value == null*/) {
+    messages.push("Enter a due date");
+  }
+
+  if (messages.length > 0) {
+    //e.preventDefault();
+    const errorElement = document.querySelector("#errorElement");
+    errorElement.innerHTML = messages.join(", ");
+  }
+
+  // Get the values of the inputs
+  const name = newTaskNameInput.value;
+  const description = newTaskDescription.value;
+  const assignedTo = newTaskAssignedTo.value;
+  const dueDate = newTaskDueDate.value;
+  /*if(!validFormFieldInput(name)){
        errorMessage.innerHTML = "Invalid name input";
        errorMessage.style.display = "block"
     }else{
         errorMessage.style.display = "none"
     }*/
-taskManager.addTask(name, description, assignedTo, dueDate);
+  taskManager.addTask(name, description, assignedTo, dueDate);
 
-//taskManager.addTask(nameValue,descriptionValue,assignedToValue,dueDateValue);
-//const taskHtml = createTaskHtml('Group2', 'wash dishs','nebyu','9/20/2022');
-//console.log(taskHtml);
+  //taskManager.addTask(nameValue,descriptionValue,assignedToValue,dueDateValue);
+  //const taskHtml = createTaskHtml('Group2', 'wash dishs','nebyu','9/20/2022');
+  //console.log(taskHtml);
 
+  // Save the tasks to localStorage
+  //taskManager.save();
 
-// Save the tasks to localStorage
-//taskManager.save();
+  // Render the tasks
+  taskManager.render();
 
-// Render the tasks
-taskManager.render();
-
-
-
-
-
-// Clear the form
-newTaskNameInput.value = '';
-newTaskDescription.value = '';
-newTaskAssignedTo.value = '';
-newTaskDueDate.value = '';
+  // Clear the form
+  newTaskNameInput.value = "";
+  newTaskDescription.value = "";
+  newTaskAssignedTo.value = "";
+  newTaskDueDate.value = "";
 });
 
 /*function validFormFieldInput(data){
@@ -104,37 +97,31 @@ newTaskDueDate.value = '';
 
 // Add the task to the task manager
 
-
-
 // Select the Tasks List
-const tasksList = document.querySelector('#tasksList');
+const tasksList = document.querySelector("#tasksList");
 
 // Add an 'onclick' event listener to the Tasks List
-tasksList.addEventListener('click', (event) => {
-
+tasksList.addEventListener("click", (event) => {
   // Check if a "Mark As Done" button was clicked
 
-  if (event.target.classList.contains('done-button')) {
+  if (event.target.classList.contains("done-button")) {
+    // Get the parent Task
 
-      // Get the parent Task
-      
-      const parentTask = event.target.parentElement.parentElement;
+    const parentTask = event.target.parentElement.parentElement;
 
-      // Get the taskId of the parent Task.
-      const taskId = Number(parentTask.dataset.taskId);
+    // Get the taskId of the parent Task.
+    const taskId = Number(parentTask.dataset.taskId);
 
-      // Get the task from the TaskManager using the taskId
-      const task = taskManager.getTaskById(taskId);
+    // Get the task from the TaskManager using the taskId
+    const task = taskManager.getTaskById(taskId);
 
-        // Update the task status to 'DONE'
-        task.status = 'DONE';
+    // Update the task status to 'DONE'
+    task.status = "DONE";
 
-        // Save the tasks to localStorage
-        taskManager.save();
+    // Save the tasks to localStorage
+    taskManager.save();
 
-        // Render the tasks
-        taskManager.render();
-    }
-  });
-
-
+    // Render the tasks
+    taskManager.render();
+  }
+});
