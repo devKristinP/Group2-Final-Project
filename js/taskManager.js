3de8e2f87def9773b1f4e735288bbfa119efe131
KristinP <devKristinP@gmail.com>
2022-09-19 20:48:57 -0600

    delete button visible now

--- js/taskManager.js
+++ js/taskManager.js
@@ -25,7 +25,7 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => `
         <div class="d-flex w-100 mt-2 justify-content-between align-items-center">
             <h5>${name}</h5>
             <span class="badge ${
-              status === 'TODO' ? 'badge-danger' : 'badge-success'
+              status === "TODO" ? "badge-danger" : "badge-success"
             }">${status}</span>
         </div>
         <div class="d-flex w-100 mb-3 justify-content-between">
@@ -35,17 +35,18 @@ const createTaskHtml = (id, name, description, assignedTo, dueDate, status) => `
         <p>${description}</p>
         <div class="d-flex w-100 justify-content-end">
             <button class="btn btn-outline-success done-button mr-1 ${
-              status === 'TODO' ? 'visible' : 'invisible'
+              status === "TODO" ? "visible" : "invisible"
             }">Mark As Done</button>
-            <button class="btn btn-outline-danger delete-button>Delete</button>
+            <button class="btn btn-outline-danger delete-button>Delete</button><button type="button" class="btn btn-danger delete-button float-right">Delete</button>
+            </div>
         </div>
     </li>
-`
+`;
 
 class TaskManager {
   constructor(currentId = 0) {
-    this.tasks = [] //this is the task list
-    this.currentId = currentId
+    this.tasks = []; //this is the task list
+    this.currentId = currentId;
   }
   // Method-- creating a task object and the key value pairs
   addTask(name, description, assignedTo, dueDate) {
@@ -55,76 +56,76 @@ class TaskManager {
       description: description,
       assignedTo: assignedTo,
       dueDate: dueDate,
-      status: 'TODO',
-    }
+      status: "TODO",
+    };
 
-    this.tasks.push(task)
+    this.tasks.push(task);
     /*may currently return first task with ID of zero, will eventually show number increasing*/
 
-    this.#saveState() // make a change, save our stuff
+    this.#saveState(); // make a change, save our stuff
   }
   // Create the deleteTask method
 
   deleteTask(taskId) {
     // Create an empty array and store it in a new variable, newTasks
-    const newTasks = []
+    const newTasks = [];
 
     //looping over the tasks
     for (let i = 0; i < this.tasks.length; i++) {
-      const task = this.tasks[i]
+      const task = this.tasks[i];
 
       if (task.id !== taskId) {
-        newTasks.push(task)
+        newTasks.push(task);
       }
     }
 
     //set this.tasks to newTasks
-    this.tasks = newTasks
+    this.tasks = newTasks;
 
-    this.#saveState() // make a change, save our stuff
+    this.#saveState(); // make a change, save our stuff
   }
 
   // # prefix makes it private field, thus inaccessible from outside the instance
   #saveState() {
     // stuff gets saved here
-    localStorage.setItem('saved-todo-tasks', this.tasks)
-    localStorage.setItem('saved-current-id', this.currentId)
+    localStorage.setItem("saved-todo-tasks", this.tasks);
+    localStorage.setItem("saved-current-id", this.currentId);
   }
 
   getTaskById(taskId) {
     // Create a variable to store the found task
-    let foundTask
+    let foundTask;
 
     // Loop over the tasks and find the task with the id passed as a parameter
     for (let i = 0; i < this.tasks.length; i++) {
       // Get the current task in the loop
 
-      const task = this.tasks[i]
+      const task = this.tasks[i];
       // kp changed task.id === task     to     task.id === taskId
       // Check if its the right task by comparing the task's id to the id passed as a parameter
       if (task.id === taskId) {
         // Store the task in the foundTask variable
-        foundTask = task
+        foundTask = task;
       }
     }
 
     // Return the found task
-    return foundTask
+    return foundTask;
   }
   // Create the render method
   render() {
     // Create an array to store the tasks' HTML
-    const tasksHtmlList = []
+    const tasksHtmlList = [];
 
     // Loop over our tasks and create the html, storing it in the array
     for (let i = 0; i < this.tasks.length; i++) {
       // Get the current task in the loop
-      const task = this.tasks[i]
+      const task = this.tasks[i];
 
       // Format the date
-      const date = new Date(task.dueDate)
+      const date = new Date(task.dueDate);
       const formattedDate =
-        date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear()
+        date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
 
       // Create the task html
       const taskHtml = createTaskHtml(
@@ -134,54 +135,54 @@ class TaskManager {
         task.assignedTo,
         formattedDate,
         task.status
-      )
+      );
 
       // Push it to the tasksHtmlList array
-      tasksHtmlList.push(taskHtml)
+      tasksHtmlList.push(taskHtml);
     }
 
     // Create the tasksHtml by joining each item in the tasksHtmlList
 
-    const tasksHtml = tasksHtmlList.join('\n')
+    const tasksHtml = tasksHtmlList.join("\n");
 
     // Set the inner html of the tasksList on the page
-    const tasksList = document.querySelector('#tasksList')
-    tasksList.innerHTML = tasksHtml
+    const tasksList = document.querySelector("#tasksList");
+    tasksList.innerHTML = tasksHtml;
   }
 
   // Create the save method
   save() {
     // Create a JSON string of the tasks
-    const tasksJson = JSON.stringify(this.tasks)
+    const tasksJson = JSON.stringify(this.tasks);
 
     // Store the JSON string in localStorage
-    localStorage.setItem('tasks', tasksJson)
+    localStorage.setItem("tasks", tasksJson);
 
     // Convert the currentId to a string;
-    const currentId = String(this.currentId)
+    const currentId = String(this.currentId);
 
     // Store the currentId in localStorage
-    localStorage.setItem('currentId', currentId)
+    localStorage.setItem("currentId", currentId);
   }
 
   // Create the load method
   load() {
     // Check if any tasks are saved in localStorage
-    if (localStorage.getItem('tasks')) {
+    if (localStorage.getItem("tasks")) {
       // Get the JSON string of tasks in localStorage
-      const tasksJson = localStorage.getItem('tasks')
+      const tasksJson = localStorage.getItem("tasks");
 
       // Convert it to an array and store it in our TaskManager
-      this.tasks = JSON.parse(tasksJson)
+      this.tasks = JSON.parse(tasksJson);
     }
 
     // Check if the currentId is saved in localStorage
-    if (localStorage.getItem('currentId')) {
+    if (localStorage.getItem("currentId")) {
       // Get the currentId string in localStorage
-      const currentId = localStorage.getItem('currentId')
+      const currentId = localStorage.getItem("currentId");
 
       // Convert the currentId to a number and store it in our TaskManager
-      this.currentId = Number(currentId)
+      this.currentId = Number(currentId);
     }
   }
 }
